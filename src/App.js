db06b3515a757b28b144e840c5f62e22f6a85e98
Stephen <stephen@tar.cplhk.tar>
2020-01-20 17:07:08 +0800

    step3 : import redux-store and show Name in screen

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
 
-function App() {
+const App = ({ state }) => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{ state.root.Name }</h1>
     </div>
   );
+
 }
 
-export default App;
+// a map for pass redux store to props
+const mapStateToProps = state => {
+  return {
+    state
+  };
+};
+
+export default connect(mapStateToProps)(App);
